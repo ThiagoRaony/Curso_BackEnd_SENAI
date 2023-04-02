b7080f69e35bc7ba09a6a69ba0a0076f5c8d1f76
ThiagoRaony <trs.assessor@gmail.com>
2023-04-02 11:50:33 -0300

    Olá mundo PHP

---
+++ Aula PHP/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Word</title>
+</head>
+<body>
+    <h1 onclick="alert('Olá Mundo JS')">Minha primeira página php</h1>
+    <form action=""></form>
+
+    <script>
+        document.write("Olá Mundo JS 2")
+    </script>
+
+    <?php
+    echo "Olá Mundo PHP";
+    ?>
+
+</body>
+</html>
